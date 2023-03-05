acd380a1d182ed4f9e280e8a7943c51edd93fe45
Ilya Yalymov <chezeee@gmail.com>
2023-03-05 20:39:24 +0300

    Solution added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,21 @@
-// your code
-console.log("form submitted");
+const loginForm = document.querySelector("#login-form");
+
+function authorization() {
+  console.log("form submitted");
+}
+
+// Кнопка отправки задизаблена до заполнения всех полей
+function checkValidity(event) {
+  const formNode = event.target.form;
+  const isValid = formNode.checkValidity();
+
+  formNode.querySelector("#submit-button").disabled = !isValid;
+}
+
+loginForm.addEventListener("input", checkValidity);
+
+loginForm.addEventListener("submit", (event) => {
+  authorization();
+
+  event.preventDefault();
+});
